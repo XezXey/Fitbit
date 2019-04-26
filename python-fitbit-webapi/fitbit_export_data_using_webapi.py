70c185ab7533eca3a28b79a62ed210ae8840b666
XezXey <puntawat-1@hotmail.com>
2019-04-27 01:59:37 +0700

    Adding more feature data acquisition, editing manual

--- python-fitbit-webapi/fitbit_export_data_using_webapi.py
+++ python-fitbit-webapi/fitbit_export_data_using_webapi.py
@@ -5,27 +5,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import sys
+import errno
 
 # More information about api can be found at https://dev.fitbit.com/build/reference/web-api/
 
-# query_str to get heart rate in current date
-query_str = 'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec.json'
+# Reading a secret token of application to access web-api from current directory
+# Access token need to store in "access_token.txt"
+try: 
+    with open('./access_token.txt', 'r') as access_token_file:
+        secret_token = access_token_file.readlines()[0].strip('\n')
+except FileNotFoundError :
+    print('No access token file')
+    exit()
 
-# secret token of application to access web-api
-secret_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRGOFkiLCJzdWIiOiI3MkNOTjciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNTQ3NzIwMTM5LCJpYXQiOjE1NDcxMTU1Mjl9.WFJJitHH4IsKvvfzd8auPOSwJIAegWlw7rpGrT4wl5Q'
+# Get input date for specific date data acquisition : default = today
+experiment_date = input("Input date(yyyy-MM-dd) that performed an experiment(default is today) : ")
+if experiment_date == '':
+    experiment_date = 'today'
 
-
-# filepath to write json file
-path = './dataset_fitbit/' + sys.argv[1] + '/'
-filename_csv = 'fitbit_hr.csv'
-filename_json = 'fitbit_hr.json'
-start_experiment_time = sys.argv[1]
 
 # define authentication header to authenticate with fitbit server
 secret_header = {'Authorization' : 'Bearer {}'.format(secret_token)}
+# query_str_dict for each features to get data in current date
+query_str_dict = {
+    'heart' : 'https://api.fitbit.com/1/user/-/activities/heart/date/{0}/1d/1sec.json'.format(experiment_date), 
+    'steps' : 'https://api.fitbit.com/1/user/-/activities/steps/date/{0}/1d.json'.format(experiment_date), 
+    'minutesSedentary' : 'https://api.fitbit.com/1/user/-/activities/minutesSedentary/date/{0}/1d.json'.format(experiment_date), 
+    'minutesLightlyActive' : 'https://api.fitbit.com/1/user/-/activities/minutesLightlyActive/date/{0}/1d.json'.format(experiment_date), 
+    'minutesFairlyActive' : 'https://api.fitbit.com/1/user/-/activities/minutesFairlyActive/date/{0}/1d.json'.format(experiment_date), 
+    'minutesVeryActive' : 'https://api.fitbit.com/1/user/-/activities/minutesVeryActive/date/{0}/1d.json'.format(experiment_date)
+}
+# feature_name_dict for renaming the columns name for each feature
+feature_name_dict = {
+    'heart' : 'HR_fitbit', 
+    'steps' : 'Steps_fitbit',
+    'minutesSedentary' : 'Sedentary_fitbit',
+    'minutesLightlyActive' : 'LightlyActive_fitbit',
+    'minutesFairlyActive' : 'FairlyActive_fitbit',
+    'minutesVeryActive' : 'VeryActive_fitbit'
+}
 
-# HTTP get to fitbit server with query string to get the data
-response = requests.get(query_str, headers=secret_header)
+fitbit_df_dict = {} # store dataFrame of each feature
+fitbit_json_dict = {} # store json data of each feature
+
+# filepath to write json file
+path = './dataset_fitbit/' + sys.argv[1] + '/'
+filename_csv = 'fitbit_data.csv'
+filename_json = 'fitbit_data.json'
+start_experiment_time = sys.argv[1]
 
 # Try to make directory if it's not exist
 if not os.path.exists(os.path.dirname(path)):
@@ -35,18 +62,27 @@ if not os.path.exists(os.path.dirname(path)):
         if exc.errno != errno.EEXIST:
             raise
 
-# Writing data into json format file
-with open(path + start_experiment_time + filename_json, 'w') as f:
-    json.dump(response.json(), f)
 
-# Reading data into json format file
-with open(path + start_experiment_time + filename_json, 'r') as f:
-    json_data = json.load(f)
-    #print(json_data)
+fitbit_df = pd.DataFrame()
+initial_flag = 0 # Flag for merge a dataFrame (preventing the empty_df merging) ===> always assign 1st df to initial merged dataframe
+# Iterate over each keys(feature) then apply : adding date + renaming + merging
+for each_feature in query_str_dict.keys():
+    print("Data Acquisition : Feature {0}".format(each_feature))
+    fitbit_json_dict[each_feature] = requests.get(query_str_dict[each_feature], headers=secret_header).json() # send HTTPS request for using web-api and get json data
+    date = fitbit_json_dict[each_feature]['activities-{0}'.format(each_feature)][0]['dateTime'] # get date by feature
+    fitbit_df_dict[each_feature] = pd.DataFrame(fitbit_json_dict[each_feature]['activities-{0}-intraday'.format(each_feature)]['dataset']) # get data by feature
+    fitbit_df_dict[each_feature].rename(columns={'time':'Timestamp', 'value':feature_name_dict[each_feature]}, inplace=True) # renaming
+    fitbit_df_dict[each_feature]['Timestamp'] = fitbit_df_dict[each_feature]['Timestamp'].apply(lambda each_time : date + '_' + each_time) # adding date
+    # merging
+    if initial_flag == 0: # assign first df for prevent a empty dataframe
+        fitbit_df = fitbit_df_dict[each_feature]
+        initial_flag = 1
+    else : 
+        fitbit_df = pd.merge(fitbit_df, fitbit_df_dict[each_feature], on='Timestamp', how='outer', sort=True) # Merge by outer join(Union), on 'Timestamp' column and sort by 'Timestamp'
 
-# Create the dataframe of given data to contain the data and for export to csv fotmat file easily
-df = pd.DataFrame(json_data['activities-heart-intraday']['dataset'])
-#plt.plot(df.time, df.value)
-#plt.show()
+# Saving to csv file
+fitbit_df.to_csv(path + start_experiment_time + filename_csv)
+print(fitbit_df_dict)
+print(fitbit_df)
 
-df.to_csv(path + start_experiment_time + filename_csv)
+
